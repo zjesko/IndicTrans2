f662636d4a6eae242b1e90423366fa5fe854cfbd
zjesko <akshat.c2k@gmail.com>
2024-05-18 15:16:45 +0530

    add initial baseline changes

--- train.sh
+++ train.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
+#SBATCH -A research
+#SBATCH -n 40
+#SBATCH --gres=gpu:4
+#SBATCH --mem-per-cpu=2048
+#SBATCH --time=4-00:00:00
+#SBATCH --output=logs.out
+#SBATCH --mail-type=END
+
+rm -rf /scratch/jesko/proto50
+mkdir -p /scratch/jesko/proto50
+cp -r proto50/model /scratch/jesko/proto50/
+
+ls /scratch/jesko/proto50/model
+
+module add u18/cuda/12.1
+module add u18/cudnn/8.4.0-cuda-11.6
+
+source ~/.bashrc
+
 # This script trains the translation model on the binarized data using fairseq.
 
 
 echo `date`
-exp_dir=$1                              # path of the experiment directory
-model_arch=${2:-"transformer_18_18"}    # model architecture (defaults to `transformer_18_18`)
+exp_dir="proto50"                              # path of the experiment directory
+out_dir="/scratch/jesko/proto50"
+model_arch=${2:-"transformer_med"}    # model architecture (defaults to `transformer_18_18`)
 
 fairseq-train $exp_dir/final_bin \
 --max-source-positions=256 \
@@ -13,7 +33,7 @@ fairseq-train $exp_dir/final_bin \
 --source-lang=SRC \
 --target-lang=TGT \
 --max-update=1000000 \
---save-interval-updates=2500 \
+--save-interval-updates=1000 \
 --arch=$model_arch \
 --activation-fn gelu \
 --criterion=label_smoothed_cross_entropy \
@@ -26,17 +46,16 @@ fairseq-train $exp_dir/final_bin \
 --lr 5e-4 \
 --warmup-updates 4000 \
 --dropout 0.2 \
---save-dir $exp_dir/model \
---keep-last-epochs 5 \
---keep-interval-updates 3 \
+--save-dir $out_dir/model \
+--keep-last-epochs 1 \
+--keep-interval-updates 1 \
 --patience 10 \
 --skip-invalid-size-inputs-valid-test \
---fp16 \
 --user-dir model_configs \
 --update-freq=32 \
 --distributed-world-size 8 \
 --num-workers 24 \
---max-tokens 1024 \
+--max-tokens 256 \
 --eval-bleu \
 --eval-bleu-args "{\"beam\": 1, \"lenpen\": 1.0, \"max_len_a\": 1.2, \"max_len_b\": 10}" \
 --eval-bleu-detok moses \
@@ -44,4 +63,8 @@ fairseq-train $exp_dir/final_bin \
 --eval-bleu-print-samples \
 --best-checkpoint-metric bleu \
 --maximize-best-checkpoint-metric \
---task translation
+--task translation \
+--wandb-project thesis-master \
+--ddp-backend=no_c10d \
+--find-unused-parameters \
+--batch-size=16
