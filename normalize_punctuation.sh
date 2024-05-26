189a3247edc71846ccaed5061d292632009959c3
zjesko <akshat.c2k@gmail.com>
2024-05-26 05:57:30 +0530

    update eval scripts

--- normalize_punctuation.sh
+++ normalize_punctuation.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 
 root=$(dirname $0)
 
-lang_map_path=$root/utils.map_token_domain.tsv
+lang_map_path=$root/utils.map_token_lang.tsv
 
 usage () {
     echo "usage: $0 lang" >&2
@@ -26,8 +26,8 @@ declare -A lang_map
 # echo $lang_map
 
 while read line; do
-    key=$(cut -f 1 <<< "$line")
-    val=$(cut -f 2 <<< "$line")
+    key=$(cut -f1 <<< "$line")
+    val=$(cut -f2 <<< "$line")
     lang_map[$key]=$val
     echo "key : $key" >&2
     echo "value : $val" >&2
@@ -38,8 +38,8 @@ if [ -v "lang_map[$lang]" ]; then
 elif [ -v "lang_map[${lang:0:3}]" ]; then
     lang=${lang_map[${lang:0:3}]}
 else
-    echo "undefined mapping: ${lang}, falling back to: ${lang:0:2}" >&2
-    lang=${lang:0:2}
+    echo "undefined mapping: ${lang}, falling back to: en" >&2
+    lang=en
 fi
 
 echo $lang >&2
